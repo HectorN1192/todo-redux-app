817e1d96a85a4f7ab547f13f170613636ff9c9e5
Hector Nieves <hnieves@devcenter.es>
2021-11-25 17:12:11 +0100

    primer commit

---
+++ src/app/filtro/filtro.reducer.ts
@@ -0,0 +1,17 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import * as actions from './filtro.actions';
+
+export const initialState: actions.filtrosValidos =
+  'todos' as actions.filtrosValidos;
+
+const _filtroReducer = createReducer(
+  initialState,
+  on(actions.setFiltro, (state, { filtro }) => filtro)
+);
+
+export function filtroReducer(
+  state: actions.filtrosValidos | undefined,
+  action: Action
+) {
+  return _filtroReducer(state, action);
+}
